411d742599ca78cbc3f8e5755bfb209e5151a711
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-10-19 11:24:17 +0530

    initial commint setup

---
+++ server/config/mongodb.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      `${process.env.MONGODB_URI}/MERN-Authentication-System`
+    );
+    console.log("✅ MongoDB connected successfully!");
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error.message);
+  }
+};
+
+export default connectDB;
---
+++ server/server.js
@@ -0,0 +1,19 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import connectDB from "./config/mongodb.js";
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ Credentials: true }));
+
+app.get("/", (req, res) => res.send("API Working"));
+
+app.listen(port, () => console.log(`Server started on PORT:${port}`));
