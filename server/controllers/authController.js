081cf96e13a28ac76e755228626923211ee2bcdc
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-10-20 11:55:26 +0530

    Implement user registration with JWT authentication
    - Added JWT_SECRET and NODE_ENV to .env
    - Added bcryptjs and updated authController to hash passwords before saving to DB
    - Implemented user registration functionality, including checking for existing users
    - Added JWT token generation and cookie setting upon successful registration

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,33 @@
+import bcrypt from "bcryptjs";
+import userModel from "../models/userModel";
+
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
     return res.json({ success: false, message: "Missing details" });
+  }
+  try {
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
+      return res.json({ success: false, message: "Missing details" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new userModel({ name, email, password: hashedPassword });
+    await user.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "nome" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
   }
 };
