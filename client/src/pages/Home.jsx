71a0820cfd3fb7d22c3c52d7cd4c35d23a4b690f
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-11-14 17:20:34 +0530

    Implement Home and Login pages with basic UI
    - Added `Header.jsx` component for the home page header section.
    - Added `Navbar.jsx` component for the navigation bar.
    - Updated `Home.jsx` to include `Navbar` and `Header` components and basic styling.
    - Added `Login.jsx` page with basic structure and placeholder content.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,13 @@
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[url('/bg_img')] bg-cover bg-center">
+      <Navbar />
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+import { assets } from "../assets/assets";
+
 const Login = () => {
-  return <div>Login Page</div>;
+  const [state, setState] = useState("sign up");
+
+  return (
+    <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400 relative">
+      <img
+        src={assets.logo}
+        alt="logo"
+        className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
+      />
+
+      <div className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm">
+        <h2 className="text-3xl font-semibold text-white text-center mb-3">
+          {state === "sign up" ? "Create Account" : "Login!"}
+        </h2>
+
+        <p className="text-center text-sm mb-6">
+          {state === "sign up"
+            ? "Create your account"
+            : "Login to your account"}
+        </p>
+        <form>
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <img src={assets.person_icon} alt="" />
+            <input
+              className="bg-transparent outline-none"
+              type="text"
+              placeholder="Full Name"
+              required
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
